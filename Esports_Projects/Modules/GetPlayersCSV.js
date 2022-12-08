aa0f746a2fa435f5f3b1b7e926ca159f12b88ae3
Mvb1122 <micahbushman.school@gmail.com>
2022-12-07 18:36:53 -0700

    Added a new module for the ESports stuff
    my club sponsor teacher told me to add this, so I did.
    
    Also, I started working on flashcards for Hiragana Teacher.

---
+++ Esports_Projects/Modules/GetPlayersCSV.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+// Process games into players by winrate.
+    // Setup data structure.
+let players = [/*{
+    Name: "ex",
+    Wins: 3,
+    Losses: 4
+}
+*/];
+
+function GetPlayerIndex(id) {
+    for (let i = 0; i < players.length; i++)
+        if (players[i].Name == id) return i;
+    
+    // If the player wasn't in the list, add them.
+    return players.push({Name: id, Wins: 0, Losses: 0}) - 1;
+}
+
+function PutMatchIn(index, wasItAWin) {
+    let data = players[index];
+
+    if (wasItAWin)
+        data.Wins++;
+    else data.Losses++;
+
+    players[index] = data;
+}
+
+// Aggregate information from files.
+let gamesPath = './Esports_Projects/Games/';
+let files = fs.readdirSync(gamesPath);
+files.forEach(file => {
+    let WasitAWin = true;
+    let data = JSON.parse(GetFileFromCache(gamesPath + file));
+    
+    if (data.Result != "Win") WasitAWin = false;
+
+    // Put in the person's game as a win or a loss.
+    data.Players.forEach(player => {
+        PutMatchIn(GetPlayerIndex(player), WasitAWin)
+    });
+
+    // Put the game in for the loser as well.
+    data.Enemies.forEach(player => {
+        PutMatchIn(GetPlayerIndex(player), !WasitAWin)
+    });
+})
+
+// Turn the data into CSV.
+let output = "name, wins, losses, winrate, reliability, \"Number of Matches\"";
+players.forEach(player => {
+    let NumberOfMatches = player.Wins + player.Losses;
+    let winrate = player.Wins / NumberOfMatches
+    let reliability = GetReliability(player.Name);
+    let PlayersActualName = JSON.parse(GetFileFromCache(`./Esports_Projects/Players/${player.Name}.json`)).Name;
+
+    output += `\n"${PlayersActualName}", ${player.Wins}, ${player.Losses}, ${winrate}, ${reliability}, ${NumberOfMatches}`;
+})
+
+res.statusCode = 200;
+
+// Tell the client to download/parse this file as CSV. 
+res.setHeader("Content-Type", "text/csv");
+res.setHeader("content-disposition", "attachment; filename=\"PlayerData.csv\"")
+res.end(output);
