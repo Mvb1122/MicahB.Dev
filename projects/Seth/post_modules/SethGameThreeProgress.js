a55e432891e8b88880b20c32c00e8f7c2f1bb752
Mvb1122 <micahbushman.school@gmail.com>
2023-11-08 12:26:32 -0700

    Create SethGameThreeProgress.js
    API for getting user progress through game three, zero indexed.

---
+++ projects/Seth/post_modules/SethGameThreeProgress.js
@@ -0,0 +1,38 @@
+/* Example Data:
+{
+    username: "SethChavez12345"
+}
+
+NOTE: Does not require password since this isn't really sensative information.
+*/
+
+const UserFolderPath = "./projects/Seth/users/", fs = require('fs');
+data = JSON.parse(data);
+const userpath = `${UserFolderPath}${data.username}.json`;
+// Load the user's file.
+if (fs.existsSync(userpath))
+    fs.readFile(userpath, (err, user) => {
+        // Parse data.
+        user = JSON.parse(user);
+
+        // Check for game three records.
+        let roundNum = 0;
+        user.rounds.forEach(round => {
+            if (round.game == 3) {
+                roundNum++;
+            }
+        });
+
+        // At the end, send it back to the user.
+        const end = {
+            successful: true,
+            roundNum: roundNum
+        };
+
+        res.end(JSON.stringify(end));
+    })
+else 
+    res.end(JSON.stringify({
+        sucessful: false,
+        reason: "Something went wrong! (Probably invalid login.)",
+    }));
