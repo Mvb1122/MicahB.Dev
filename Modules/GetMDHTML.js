258ffea5a04932eb4da35f7106c0735d1db6595e
Mvb1122 <micahbushman.school@gmail.com>
2023-12-07 17:50:04 -0700

    Create request for converting Obsidian MD to HTML
    (Obsidian Markdown)

---
+++ Modules/GetMDHTML.js
@@ -0,0 +1,56 @@
+/**
+ * args: Location 
+ */
+
+// Regexs
+const ObsidianLinkRegex = new RegExp(/\[\[[^\]]*\]\]/g);
+const TagRegex = new RegExp(/(?<!#)\B#{1}([A-Za-z0-9]{1,})/g)
+
+const fs = require('fs');
+const path = require('path');
+const Showdown = require('showdown');
+
+
+args.location = unescape(args.location.trim("\""));
+if (args.location != undefined && fs.existsSync(args.location)) {
+    // First, let's read in the file.
+    /**
+     * @type {String}
+    */
+    let file = GetFileFromCache("./" + args.location).toString(); // .split("\n");
+
+    const ObsidianLinks = file.match(ObsidianLinkRegex);
+    // Replace obsidian links.
+    for (let i = 0; i < ObsidianLinks.length; i++) try {
+        let link = ObsidianLinks[i];
+        const inside = link.substring(2, link.length - 2);
+        
+        // Find the file.
+        const Matches = FindFile(inside);
+        console.log(`{${Matches}}, with term ${inside}, replacing ${link}`);
+        file = file.replaceAll(link, `[${inside}](./${Matches[0]})`);
+    } catch { ; } // Do nothing.
+
+    // Replace tags.
+    const tags = file.match(TagRegex);
+    for (let i = 0; i < tags.length; i++ ) {
+        let tag = tags[i];
+        const inside = tag.substring(1);
+        file = file.replaceAll(tag, `{TAG:${inside}}`);
+    }
+
+    // Hand it over to Showdown for the actual conversion.
+    const Converter = new Showdown.Converter();
+    file = Converter.makeHtml(file);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", getMime("html"));
+    res.end(file);
+} else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", getMime("json"));
+    res.end(JSON.stringify({
+        sucessful: false,
+        reason: "Invalid file!"
+    }));
+}
