d36f066b4ea22746a18e2e7d2e3b160e175cdda7
Mvb1122 <micahbushman.school@gmail.com>
2023-12-11 00:51:56 -0700

    small tweaks + backup

--- Hiragana_Teacher/Post_Modules/GetExampleSentence.js
+++ Hiragana_Teacher/Post_Modules/GetExampleSentence.js
@@ -62,7 +62,7 @@ if (ShouldGenerate) {
     
     const data = JSON.stringify({
         role: "user",
-        content: `Please write a very basic Japanese example sentence for the following word or phrase. Feel free to write a dialogue, but keep it simple. Do not write anything except for the example sentence. After you have written your example, please write an English translation beneath it. Only write your translation once. Phrase: ${submitted.Word}\nWith the definition ${submitted.Definition}`
+        content: `Please write a Japanese example sentence for the following word or phrase. Feel free to write a dialogue, but keep it simple. Do not write anything except for the example sentence. After you have written your example, please write an English translation beneath it. Only write your translation once. Phrase: ${submitted.Word}\nWith the definition ${submitted.Definition}. Please do not write any notes onto the end of your translation.`
     });
     
     const response = await fetch(url, {
