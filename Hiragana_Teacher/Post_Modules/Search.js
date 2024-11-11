340f5b300eacf11df46ad46d4c0cfa9d1401a22c
Mvb1122 <micahbushman.school@gmail.com>
2024-11-11 09:40:49 -0800

    Mermaid Graph support for ObsidianViewer
    + IP changes
    + Backup
    + New Search post module.

---
+++ Hiragana_Teacher/Post_Modules/Search.js
@@ -0,0 +1,69 @@
+const fs = require('fs'); const fp = require('fs/promises');
+/* Example input data:
+{
+    "term": "Genki II",
+    "limit": 5 // Default 5.
+}
+
+Example output data:
+{
+    results: [ // Results sorted by similarity to term.
+        {name: "Genki 2", id: 1234, score: 1234 }
+    ]
+}
+*/
+
+/**
+ * 
+ * @param {string} string 
+ * @param {string[]} terms 
+ * @returns {Number}
+ */
+function SimilarityToTerms(string, terms) {
+    const stringWords = string.toLowerCase().split(" ");
+
+    // Split by words and return how many are included in the term. 
+    let o = 0;
+    for (let i = 0; i < terms.length; i++) {
+        const part = terms[i];
+        if (stringWords.includes(part.toLowerCase())) o++;
+    }
+    
+    return o;
+}
+
+// Check if that person's progress exists.
+const SetsDir = __dirname + "/Hiragana_Teacher/Sets/";
+res.statusCode = 200;
+res.setHeader("Content-Type", "application/json");
+let postData = JSON.parse(data)
+if (postData.term) {
+    if (!postData.limit) postData.limit = 5;
+
+    const splitTerm = postData.term.toLowerCase().split(" ");
+
+    // Use a promise array to asynchronously read all files.
+        // Speeds up response time.
+        
+    const promises = fs.readdirSync(SetsDir).map(async v => {
+        // v is filename.
+        const data = JSON.parse(await fp.readFile(SetsDir + v));
+
+        return {
+            "name": data.Name,
+            "id": v,
+            "score": SimilarityToTerms(data.Name, splitTerm)
+        }
+    });
+
+    const results = (await Promise.all(promises))
+        .sort((a, b) => b.score - a.score) // Sort low to high, take highest few.
+        .slice(0, postData.limit);
+
+    return res.end(JSON.stringify({
+        "results": results
+    }));
+
+} else 
+    // Send back an empty result series if there's no 
+    return res.end(JSON.stringify({"results":[]}));
--- index.js
+++ index.js
@@ -632,7 +632,7 @@ const requestListener = async function (req, res) {
     }
 };
 
-const host = '192.168.1.3' // require('os').hostname();
+const host = '192.168.68.58' // require('os').hostname();
 const port = 80;
 
 const server = http.createServer(requestListener);
